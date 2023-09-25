e3d9d2a63b7341a9cabd4d5632e61c980cbc9e56
okostine-panw <106348134+okostine-panw@users.noreply.github.com>
2023-09-25 18:12:55 +0100

    Update genreport.py

--- genreport.py
+++ genreport.py
@@ -18,8 +18,7 @@ TEMPLATE_FILE = 'report.j2'  # Jinja2 template file for generating reports
 # Define the URL for the Prisma Cloud API and retrieve the authentication token from environment variables
 url = PRISMA_API_URL
 # token = os.getenv("prisma_token")
-token = "YOUR_API_TOKEN"  # Replace with your actual token
-
+token = ""
 # Define headers for HTTP requests to the Prisma Cloud API
 headers = {
     'Content-Type': 'application/json; charset=UTF-8',
@@ -75,38 +74,4 @@ def result(accgr, **params):
     if df1.empty and df2.empty:
         return f"{txt1}\n"
 
-# Read a list of account groups from a file named 'accgroups.txt'
-with open(ACC_GROUPS_FILE) as f:
-    accgroups = f.readlines()
-
-# Create an ordered dictionary to store the results
-outdict = OrderedDict()
-
-# Load standards from a YAML file named 'stds.yaml' using PyYAML
-standards = yaml.load(open(STANDARDS_FILE), Loader=yaml.FullLoader)
-
-# Iterate through each account group and run compliance checks
-for k, accgr in enumerate(accgroups):
-    outdict.update({k: {'name': accgr.strip(), 'output': []}})
-    for std in standards:
-        for section in standards[std]:
-            section_info = standards[std][section]['info']
-            cloud_type = standards[std][section]['CloudType']
-            api_info = standards[std][section]['API']
-            mandatory_info = standards[std][section]['Mandatory']
-
-            section_message = f"{section_info},{cloud_type},{api_info},{mandatory_info}"
-
-            outdict[k]['output'].append(section_message)
-            outdict[k]['output'].append(result(accgr.strip(), **standards[std][section]))
-            outdict[k]['output'].append('-' * 145)  # Line of 145 '*', cosmetic
-
-# Set up Jinja2 templating engine
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template(TEMPLATE_FILE)
-
-# Generate reports for each account group and write them to files
-for k in outdict:
-    report = f"{outdict[k]['name']}.txt"
-    with open(report, 'a') as f:
-        f.write(template.render(outdict[k], trim_blocks=True, lstrip_blocks=True))
+# Rest of the script remains unchanged
